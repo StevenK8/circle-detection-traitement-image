2b98f71647eeba1440d51300e5a02789cf0f02a9
Steven <steven928@hotmail.fr>
2021-10-18 10:01:00 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+	cout << "TP2\n";
+
+	return 0;
+}
