e9b85b603544da11dfba58b4c648f71a12f73061
Steven <steven928@hotmail.fr>
2021-10-31 02:10:06 +0200

    final version with working circle detection

--- main.cpp
+++ main.cpp
@@ -22,7 +22,8 @@ struct accumulator
 
 struct CircleStruct
 {
-	int x, y, r, v;
+	int x, y, r;
+	double v;
 };
 
 bool compareByValue(const CircleStruct &a, const CircleStruct &b)
@@ -35,7 +36,7 @@ double deg2rad(double degrees)
 	return degrees * 4.0 * atan(1.0) / 180.0;
 }
 
-auto HoughTransform(unsigned char *img_data, int w, int h, accumulator accu)
+auto HoughTransform(unsigned char *img_data, int w, int h, accumulator accu, int rmin, int rmax)
 {
 	cout << "\rRecherche de cercles \n";
 	for (int y = 0; y < h; y++)
@@ -45,7 +46,7 @@ auto HoughTransform(unsigned char *img_data, int w, int h, accumulator accu)
 		{
 			if (img_data[(y * w) + x] > 25)
 			{
-				for (int r = 1; r < sqrt(pow(w, 2) + pow(h, 2)); r++)
+				for (int r = rmin; r < min((double)rmax, sqrt(pow(w, 2) + pow(h, 2))); r++)
 				{
 					for (int t = 0; t < 360; t++)
 					{
@@ -67,34 +68,42 @@ auto HoughTransform(unsigned char *img_data, int w, int h, accumulator accu)
 vector<CircleStruct> AccumulatorThreshold(accumulator accu, double threshold)
 {
 	vector<CircleStruct> circles;
-	for (int x = 2; x < accu.accu.size() - 2; x += 4)
+	for (int x = 2; x < accu.accu.size() - 2; x++)
 	{
-		for (int y = 2; y < accu.accu[x].size() - 2; y += 4)
+		for (int y = 2; y < accu.accu[x].size() - 2; y++)
 		{
-			int max = 0;
-			CircleStruct circleMax = {0, 0, 0};
-			for (int r = 7; r < accu.accu[x][y].size() - 2; r += 4)
+			for (int r = 5; r < accu.accu[x][y].size() - 2; r++)
 			{
-
+				int val = accu.accu[x][y][r];
+				bool isCenter = true;
+				// CircleStruct circleMax = {0, 0, 0};
 				for (int xnear = x - 2; xnear < x + 2; xnear++)
 				{
 					for (int ynear = y - 2; ynear < y + 2; ynear++)
 					{
 						for (int rnear = r - 2; rnear < r + 2; rnear++)
 						{
-							if (accu.accu[xnear][ynear][rnear] > max)
+							if (accu.accu[xnear][ynear][rnear] > val)
 							{
-								max = accu.accu[xnear][ynear][rnear];
-								circleMax = {xnear, ynear, rnear, max};
+								isCenter = false;
+								break;
+								// max = accu.accu[xnear][ynear][rnear];
+								// circleMax = {xnear, ynear, rnear, max};
+							}
+							else if (accu.accu[xnear][ynear][rnear] == val)
+							{
+								accu.accu[x][y][r]++;
+								break;
 							}
 						}
 					}
 				}
-			}
-			if (accu.accu[circleMax.x][circleMax.y][circleMax.r] > threshold)
-			{
-				// cout << circleMax.v << "\n";
-				circles.push_back({circleMax.x, circleMax.y, circleMax.r, circleMax.v});
+				if (isCenter && val > threshold)
+				{
+					// cout << circleMax.v << "\n";
+					CircleStruct c = {x, y, r, accu.accu[x][y][r]};
+					circles.push_back(c);
+				}
 			}
 		}
 	}
@@ -124,23 +133,22 @@ Mat preprocessImage(Mat img)
 	return grad;
 }
 
-void showCircles(Mat img, vector<CircleStruct> circles, int number_of_circles)
+void showCircles(Mat img, vector<CircleStruct> circles, int number_of_circles, int scale)
 {
 	sort(circles.begin(), circles.end(), compareByValue);
 	for (size_t i = 0; i < min((int)(circles.size()), number_of_circles); i++)
 	{
-		cout << circles[i].v << "\n";
-		Point center(cvRound(circles[i].x), cvRound(circles[i].y));
-		int radius = cvRound(circles[i].r);
+		cout << "x:" << circles[i].x * scale << " y:" << circles[i].y * scale << " r:" << circles[i].r * scale << " v:" << circles[i].v << "\n";
+		Point center(cvRound(circles[i].x) * scale, cvRound(circles[i].y) * scale);
+		int radius = cvRound(circles[i].r * scale);
 		circle(img, center, radius, Scalar(0, 0, 255), 1, 8, 0);
 		circle(img, center, 0, Scalar(0, 255, 0), 1, 8, 0);
 	}
 }
 
-Mat HoughCirclesProcess(Mat img, int number_of_circles)
+vector<CircleStruct> HoughCirclesProcess(Mat img, int number_of_circles, int rmin, int rmax)
 {
 	Mat grad = preprocessImage(img); // Preprocessing
-
 	// threshold(grad, grad, 25, 255, THRESH_BINARY);
 
 	// Initialisation de l'accumulateur
@@ -148,17 +156,10 @@ Mat HoughCirclesProcess(Mat img, int number_of_circles)
 	accumulator accu, accu2;
 	accu.accu = vector<vector<vector<double>>>(grad.cols, vector<vector<double>>(grad.rows, vector<double>(r)));
 
-	auto start = chrono::high_resolution_clock::now();
+	accu = HoughTransform(grad.data, grad.cols, grad.rows, accu, rmin, rmax); //Détection de cercles
+	vector<CircleStruct> circles = AccumulatorThreshold(accu, 260);			  //Filtrage
 
-	accu = HoughTransform(grad.data, grad.cols, grad.rows, accu);	//Détection de cercles
-	vector<CircleStruct> circles = AccumulatorThreshold(accu, 250); //Filtrage
-	showCircles(img, circles, number_of_circles);
-
-	auto finish = std::chrono::high_resolution_clock::now();
-	chrono::duration<double> totalTime = finish - start;
-	cout << "Temps total: " << (int)(totalTime.count() * 1000) << " ms" << std::endl;
-
-	return img;
+	return circles;
 }
 
 int main(int argc, char **argv)
@@ -197,14 +198,30 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	auto start = chrono::high_resolution_clock::now();
+
 	if (img.cols > 150 && img.rows > 150)
 	{
+		int rmin = 13;
+		int rmax = 20;
 		// optimisation dans le cas d'une image plus grande
-		auto originalImage = img;
-		resize(img, img, Size(img.cols / 2, img.rows / 2), INTER_LINEAR);
+		auto smallImage = img;
+		resize(img, smallImage, Size(img.cols / 2, img.rows / 2), INTER_LINEAR);
+
+		auto circlesSmall = HoughCirclesProcess(smallImage, number_of_circles, rmin, rmax);
+		auto circlesNormal = HoughCirclesProcess(img, number_of_circles, 1, rmin * 2);
+		showCircles(img, circlesSmall, number_of_circles / 2, 2);
+		showCircles(img, circlesNormal, number_of_circles % 2 == 0 ? number_of_circles / 2 : (number_of_circles + 1) / 2, 1);
+	}
+	else
+	{
+		auto circles = HoughCirclesProcess(img, number_of_circles, 1, 30);
+		showCircles(img, circles, number_of_circles, 1);
 	}
 
-	img = HoughCirclesProcess(img, number_of_circles);
+	auto finish = std::chrono::high_resolution_clock::now();
+	chrono::duration<double> totalTime = finish - start;
+	cout << "Temps total: " << (int)(totalTime.count() * 1000) << " ms" << std::endl;
 
 	// Affichage
 	resize(img, img, Size(img.cols * 8, img.rows * 8), INTER_LINEAR);
